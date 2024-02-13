ee9573fa4efb7dc3d6b9c6a7177a30206d746709
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2024-02-13 20:13:10 +0600

    Add files via upload

---
+++ Multiplier/Multiplier/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Multiplier
+{
+    private int factor; // Instance variable to store the multiplier
+
+    public Multiplier(int number)
+    {
+        factor = number; // Initialize the instance variable in the constructor
+    }
+
+    public int Multiply(int number)
+    {
+        int result = factor * number;
+        factor = result; // Update the instance variable with the new result
+        return result;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Multiplier multiplyByThree = new Multiplier(3);
+        Console.WriteLine("multiplyByThree.Multiply(2): " + multiplyByThree.Multiply(2));
+        Multiplier multiplyByFour = new Multiplier(4);
+        Console.WriteLine("multiplyByFour.Multiply(2): " + multiplyByFour.Multiply(2));
+        Console.WriteLine("multiplyByThree.Multiply(1): " + multiplyByThree.Multiply(1));
+        Console.WriteLine("multiplyByFour.Multiply(1): " + multiplyByFour.Multiply(1));
+        Console.WriteLine("multiplyByFour.Multiply(3): " + multiplyByFour.Multiply(3));
+    }
+}
---
+++ Payment Card/Payment Card/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercise_106
+{
+    public class PaymentCard
+    {
+        private double balance;
+
+        public PaymentCard(double openingBalance)
+        {
+            // Constructor to initialize the balance with the opening balance
+            balance = openingBalance;
+        }
+
+        public override string ToString()
+        {
+            // Override the ToString method to return the card's balance
+            return "The card has a balance of " + balance + " euros";
+        }
+
+        public static void Main(string[] args)
+        {
+            PaymentCard card = new PaymentCard(50);
+            Console.WriteLine(card);
+        }
+    }
+}
